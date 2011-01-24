a2d532bf88db15ef5236a3f311fb424f97e64f58
@pastak <pasta0915@gmail.com>
2011-01-24 17:10:35 +0900

    version up 0.92 (Add OptionMenu)

--- src/net/cosmio/pastak/android/redsheet/Makenew.java
+++ src/net/cosmio/pastak/android/redsheet/Makenew.java
@@ -16,10 +16,13 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Makenew extends Activity{
 	public Context me=this;
 	public String SaveFileName;
+	public EditText editor;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,7 +31,7 @@ public class Makenew extends Activity{
 		ilayout.setOrientation(LinearLayout.VERTICAL);
 		TextView text=new TextView(this);
 		text.setText("赤シートにしたい部分を?で囲んで下さい");
-		final EditText editor=new EditText(this);
+		editor=new EditText(this);
 		editor.setMinLines(10);
 		editor.setMaxLines(15);
 		editor.setGravity(Gravity.TOP);
@@ -37,46 +40,72 @@ public class Makenew extends Activity{
 		button.setOnClickListener(new View.OnClickListener(){
 			@Override
 			public void onClick(View v) {
-				final String s=editor.getText().toString();
-					final EditText edtInput=new EditText(me);
-					new AlertDialog.Builder(me)
-					.setTitle("file name")
-			        .setMessage("plz file name to save\n (only use alphabet and number)")
-			        .setView(edtInput)
-			        .setPositiveButton("OK", new DialogInterface.OnClickListener(){
-			        	public void onClick(DialogInterface dialog, int whichButton){
-			        		SaveFileName=edtInput.getText().toString();
-			        		try{
-			        			OutputStream out = openFileOutput(SaveFileName+".txt",MODE_PRIVATE);
-								PrintWriter writer =
-								new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
-								writer.append(s);
-								writer.close();
-								new AlertDialog.Builder(me)
-						    	.setTitle("Success!")
-						    	.setMessage("Make New Sheet!!\n Back to Top?")
-						    	.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog, int which) {
-										Intent intent=new Intent(me,redsheet.class);
-										intent.setAction(Intent.ACTION_VIEW);
-										Makenew.this.startActivity(intent);
-									}
-						    	})
-						    	.setNegativeButton("make more", new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog, int which) {
-										editor.setText("");
-									}
-								})
-						    	.show();
-			        		}catch(IOException e){
-								e.printStackTrace();
-							}
-			        	}	
-			        }).show();
+					Makenew.this.Save();
 			}
 		});
 		ilayout.addView(text);
 		ilayout.addView(editor);
 		ilayout.addView(button);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu);
+		menu.add(0,0,Menu.NONE,"save")
+		.setIcon(android.R.drawable.ic_menu_save);
+		menu.add(0,1,Menu.NONE,"Back to Top")
+		.setIcon(android.R.drawable.ic_menu_revert);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	switch (item.getItemId()) {
+		case 0:
+			Makenew.this.Save();
+			return true;
+		case 1: 
+			Intent intent=new Intent(me,redsheet.class);
+			intent.setAction(Intent.ACTION_VIEW);
+			Makenew.this.startActivity(intent);
+			return true;
+	}
+	return super.onOptionsItemSelected(item);
+	}
+	public void Save(){
+		final String s=editor.getText().toString();
+		final EditText edtInput=new EditText(me);
+		new AlertDialog.Builder(me)
+		.setTitle("file name")
+        .setMessage("plz file name to save\n (only use alphabet and number)")
+        .setView(edtInput)
+        .setPositiveButton("OK", new DialogInterface.OnClickListener(){
+        	public void onClick(DialogInterface dialog, int whichButton){
+        		SaveFileName=edtInput.getText().toString();
+        		try{
+        			OutputStream out = openFileOutput(SaveFileName+".txt",MODE_PRIVATE);
+					PrintWriter writer =
+					new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
+					writer.append(s);
+					writer.close();
+					new AlertDialog.Builder(me)
+			    	.setTitle("Success!")
+			    	.setMessage("Make New Sheet!!\n Back to Top?")
+			    	.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							Intent intent=new Intent(me,redsheet.class);
+							intent.setAction(Intent.ACTION_VIEW);
+							Makenew.this.startActivity(intent);
+						}
+			    	})
+			    	.setNegativeButton("make more", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							editor.setText("");
+						}
+					})
+			    	.show();
+        		}catch(IOException e){
+					e.printStackTrace();
+				}
+        	}	
+        }).show();
 	}
 }
--- src/net/cosmio/pastak/android/redsheet/sheetdownload.java
+++ src/net/cosmio/pastak/android/redsheet/sheetdownload.java
@@ -12,6 +12,9 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.util.EntityUtils;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -68,52 +71,7 @@ public class sheetdownload extends Activity{
         button.setOnClickListener(new View.OnClickListener(){
         	@Override
 			public void onClick(View v) {
-				EditText et=(EditText)findViewById(R.id.EditText01);
-				final String s=et.getText().toString();
-					final EditText edtInput=new EditText(me);
-					new AlertDialog.Builder(me)
-					.setTitle("file name")
-			        .setMessage("plz file name to save\n(only use alphabet and number)")
-			        .setView(edtInput)
-			        .setPositiveButton("OK", new DialogInterface.OnClickListener(){
-			        	public void onClick(DialogInterface dialog, int whichButton){
-			        		SaveFileName=edtInput.getText().toString();
-			        		OutputStream out;
-							try {
-								out = openFileOutput(SaveFileName+".txt",MODE_PRIVATE);
-								PrintWriter writer =
-								new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
-								writer.append(s);
-								writer.close();
-								new AlertDialog.Builder(me)
-						    	.setTitle("Success!")
-						    	.setMessage("Download New Sheet!!\n go to view this sheet?")
-						    	.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog, int which) {
-										Intent intent=new Intent(me,SheetView.class);
-										intent.putExtra("filename", SaveFileName+".txt");
-										intent.setAction(Intent.ACTION_VIEW);
-										sheetdownload.this.startActivity(intent);
-									}
-						    	})
-						    	.setNeutralButton("Back to Top", new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog, int which) {
-										Intent intent=new Intent(me,redsheet.class);
-										intent.setAction(Intent.ACTION_VIEW);
-										sheetdownload.this.startActivity(intent);
-									}
-						    	})
-						    	.setNegativeButton("download more", new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog, int which) {
-										Intent intent=new Intent(me,sheetdownload.class);
-										intent.setAction(Intent.ACTION_VIEW);
-										sheetdownload.this.startActivity(intent);
-									}
-								})
-						    	.show();
-							} catch (FileNotFoundException e) {} catch (IOException e) {}
-			        	}
-			        }).show();
+				sheetdownload.this.Save();
 			}
 		});
 	}
@@ -132,5 +90,76 @@ public class sheetdownload extends Activity{
 	    {
 	        return null;
 	    }
-	}	
+	}
+	public void Save(){
+		EditText et=(EditText)findViewById(R.id.EditText01);
+		final String s=et.getText().toString();
+			final EditText edtInput=new EditText(me);
+			new AlertDialog.Builder(me)
+			.setTitle("file name")
+	        .setMessage("plz file name to save\n(only use alphabet and number)")
+	        .setView(edtInput)
+	        .setPositiveButton("OK", new DialogInterface.OnClickListener(){
+	        	public void onClick(DialogInterface dialog, int whichButton){
+	        		SaveFileName=edtInput.getText().toString();
+	        		OutputStream out;
+					try {
+						out = openFileOutput(SaveFileName+".txt",MODE_PRIVATE);
+						PrintWriter writer =
+						new PrintWriter(new OutputStreamWriter(out,"UTF-8"));
+						writer.append(s);
+						writer.close();
+						new AlertDialog.Builder(me)
+				    	.setTitle("Success!")
+				    	.setMessage("Download New Sheet!!\n go to view this sheet?")
+				    	.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								Intent intent=new Intent(me,SheetView.class);
+								intent.putExtra("filename", SaveFileName+".txt");
+								intent.setAction(Intent.ACTION_VIEW);
+								sheetdownload.this.startActivity(intent);
+							}
+				    	})
+				    	.setNeutralButton("Back to Top", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								Intent intent=new Intent(me,redsheet.class);
+								intent.setAction(Intent.ACTION_VIEW);
+								sheetdownload.this.startActivity(intent);
+							}
+				    	})
+				    	.setNegativeButton("download more", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								Intent intent=new Intent(me,sheetdownload.class);
+								intent.setAction(Intent.ACTION_VIEW);
+								sheetdownload.this.startActivity(intent);
+							}
+						})
+				    	.show();
+					} catch (FileNotFoundException e) {} catch (IOException e) {}
+	        	}
+	        }).show();
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu);
+		menu.add(0,0,Menu.NONE,"save")
+		.setIcon(android.R.drawable.ic_menu_save);
+		menu.add(0,1,Menu.NONE,"Back to Top")
+		.setIcon(android.R.drawable.ic_menu_revert);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	switch (item.getItemId()) {
+		case 0:
+			sheetdownload.this.Save();
+			return true;
+		case 1: 
+			Intent intent=new Intent(me,redsheet.class);
+			intent.setAction(Intent.ACTION_VIEW);
+			sheetdownload.this.startActivity(intent);
+			return true;
+	}
+	return super.onOptionsItemSelected(item);
+	}
 }
